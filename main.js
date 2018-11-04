fe06d43e5ce411e7101aad4381c55ee63fd9f56e
TrendBench <41747585+TrendBench@users.noreply.github.com>
2018-11-04 09:32:25 -0600

    Update main.js

--- main.js
+++ main.js
@@ -4,7 +4,7 @@ var http = require('http');
 // configure our HTTP server
 var server = http.createServer(function (request, response) {
   response.writeHead(200, {"Content-Type": "text/plain"});
-  response.end("Hello Human! I am here to announce success!\n");
+  response.end("If you change it, they will build!\n");
 });
 
 // listen on localhost:8000
