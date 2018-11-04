455ee94e97549313b48e5277341ee3889aa5aaf6
TrendBench <41747585+TrendBench@users.noreply.github.com>
2018-11-04 16:58:09 -0600

    Update main.js

--- main.js
+++ main.js
@@ -4,7 +4,7 @@ var http = require('http');
 // configure our HTTP server
 var server = http.createServer(function (request, response) {
   response.writeHead(200, {"Content-Type": "text/plain"});
-  response.end("Yo, we good now?\n");
+  response.end("Ain't no party like a CI/CD party!\n");
 });
 
 // listen on localhost:8000
